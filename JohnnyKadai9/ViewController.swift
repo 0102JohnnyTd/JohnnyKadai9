975e7aab8a701b1079e1357d078b0c040b2c0a4d
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-16 07:52:52 +0900

    【Main.storyboard】storyboard上のオブジェクトを@IBOutletでファイルに接続

--- JohnnyKadai9/ViewController.swift
+++ JohnnyKadai9/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var prefectureLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
