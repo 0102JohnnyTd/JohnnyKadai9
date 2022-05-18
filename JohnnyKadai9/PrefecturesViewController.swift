5ed2b7670b4ef496852d5e868e6fd5d8b863d257
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-19 07:49:27 +0900

    【prefectures.storyboard】クラスファイルを作成

---
+++ JohnnyKadai9/PrefecturesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PrefecturesViewController.swift
+//  JohnnyKadai9
+//
+//  Created by Johnny Toda on 2022/05/19.
+//
+
+import UIKit
+
+class PrefecturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
