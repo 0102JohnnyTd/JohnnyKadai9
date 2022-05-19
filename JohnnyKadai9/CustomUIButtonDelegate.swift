4095e176aa517b462665ad6bc693060960fa9076
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-20 07:36:49 +0900

    値の橋渡しを実行するプロトコルを定義

---
+++ JohnnyKadai9/CustomUIButtonDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CustomUIButtonDelegate.swift
+//  JohnnyKadai9
+//
+//  Created by Johnny Toda on 2022/05/19.
+//
+
+import Foundation
+import UIKit
+
+protocol CustomUIButtonDelegate {
+    func savePrefecturesLabel(button: UIButton)
+}
