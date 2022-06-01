4ca9e30db0bf726a644d669b12f34cee619f3053
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-01 20:24:43 +0900

    ハードコーディングだった箇所を修正

--- JohnnyKadai9/PrefecturesViewController.swift
+++ JohnnyKadai9/PrefecturesViewController.swift
@@ -7,34 +7,37 @@
 
 import UIKit
 
+
 final class PrefecturesViewController: UIViewController {
     @IBOutlet private weak var tokyoLabelButton: UIButton!
     @IBAction private func passTokyoData(_ sender: Any) {
-        delegate?.savePrefecturesLabel(prefecturesData: tokyoLabelButton.titleLabel?.text ?? "取得エラーです")
+        delegate?.savePrefecturesLabel(prefecturesData: tokyoLabelButton.titleLabel?.text ?? getError)
             dismiss(animated: true, completion: nil)
     }
 
     @IBOutlet private weak var kanagawaLabelButton: UIButton!
     @IBAction private func passKanagawaData(_ sender: Any) {
-        delegate?.savePrefecturesLabel(prefecturesData: kanagawaLabelButton.titleLabel?.text ?? "取得エラーです")
+        delegate?.savePrefecturesLabel(prefecturesData: kanagawaLabelButton.titleLabel?.text ?? getError)
         dismiss(animated: true, completion: nil)
     }
 
     @IBOutlet private weak var saitamaLabelButton: UIButton!
     @IBAction private func passSaitamaData(_ sender: Any) {
-        completion?(saitamaLabelButton.titleLabel?.text ?? "取得エラーです")
+        completion?(saitamaLabelButton.titleLabel?.text ?? getError)
         dismiss(animated: true)
     }
 
     @IBOutlet private weak var chibaLabelButton: UIButton!
     @IBAction private func passChibaData(_ sender: Any) {
-        completion?(chibaLabelButton.titleLabel?.text ?? "取得エラーです")
+        completion?(chibaLabelButton.titleLabel?.text ?? getError)
         dismiss(animated: true)
     }
 
     @IBAction private func quit(_ sender: Any) {
         dismiss(animated: true)
     }
+
+    private let getError = "取得エラーです"
 
     weak var delegate: CustomUIButtonDelegate?
     var completion: ((String) -> Void)?
--- JohnnyKadai9/ViewController.swift
+++ JohnnyKadai9/ViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 final class ViewController: UIViewController {
     private let segueIdentifier = "segueId"
+    private let errorMessage = "prefecturesViewControllerを取得できませんでした。"
     @IBOutlet private weak var prefectureLabel: UILabel!
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == segueIdentifier {
             let navigationController = segue.destination as! UINavigationController
             guard let prefecturesViewController = navigationController.viewControllers[0] as? PrefecturesViewController else {
-                    print("prefecturesViewControllerを取得できませんでした。")
+                    print(errorMessage)
                     return
                 }
             prefecturesViewController.delegate = self
