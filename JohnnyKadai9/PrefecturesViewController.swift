006c86759d0d1d505f3f8e074d4cdd7bdeb47665
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-04 23:42:51 +0900

    ユーザーのアクションによって失敗が起こるわけではなく、機能していることが前提の処理であることから強制キャストでも問題はないと判断し、変更

--- JohnnyKadai9/PrefecturesViewController.swift
+++ JohnnyKadai9/PrefecturesViewController.swift
@@ -15,25 +15,25 @@ final class PrefecturesViewController: UIViewController {
 
     @IBOutlet private weak var tokyoLabelButton: UIButton!
     @IBAction private func passTokyoData(_ sender: Any) {
-        delegate?.savePrefecturesLabel(prefecturesData: tokyoLabelButton.titleLabel?.text ?? getError)
+        delegate?.savePrefecturesLabel(prefecturesData: tokyoLabelButton.titleLabel!.text!)
             dismiss(animated: true, completion: nil)
     }
 
     @IBOutlet private weak var kanagawaLabelButton: UIButton!
     @IBAction private func passKanagawaData(_ sender: Any) {
-        delegate?.savePrefecturesLabel(prefecturesData: kanagawaLabelButton.titleLabel?.text ?? getError)
+        delegate?.savePrefecturesLabel(prefecturesData: kanagawaLabelButton.titleLabel!.text!)
         dismiss(animated: true, completion: nil)
     }
 
     @IBOutlet private weak var saitamaLabelButton: UIButton!
     @IBAction private func passSaitamaData(_ sender: Any) {
-        completion?(saitamaLabelButton.titleLabel?.text ?? getError)
+        completion?(saitamaLabelButton.titleLabel!.text!)
         dismiss(animated: true)
     }
 
     @IBOutlet private weak var chibaLabelButton: UIButton!
     @IBAction private func passChibaData(_ sender: Any) {
-        completion?(chibaLabelButton.titleLabel?.text ?? getError)
+        completion?(chibaLabelButton.titleLabel!.text!)
         dismiss(animated: true)
     }
 
